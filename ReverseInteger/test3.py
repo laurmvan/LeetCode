f208f846f4873b53929fecb86a3cad2bbdd922e3
Lauren Van Vlierbergen <lvanvlierbergen@gmail.com>
2021-08-31 12:37:47 -0400

    ReverseInt

--- ReverseInteger/test3.py
+++ ReverseInteger/test3.py
@@ -2,30 +2,28 @@ import unittest
 
 def reverse(x):
     neg = False
-    if (x > pow(2,31)-1 or x < pow(-2,31) or x == 0): #check if 32 bit integer
-        return 0
     
     if x < 0: #check if negative
         neg = True
         
     x = abs(x)
-    l1 = [int(x) for x in str(x)] # make number a string to be interated
-    l1.sort(reverse=True) # reverse sort list of integers
-    
-    if (l1[-1] == 0):
-        l1 = [i for i in l1 if i!=0]
-    
+    l1 = [int(x) for x in str(x)] #make number a string to be interated
+    l1.reverse() #reverses elements in a list
+
     strings = [str(integer) for integer in l1] #makes list of string integers
     a_string = "".join(strings) #combines list of integers (type string) into one string
     x2 = int(a_string) #converts string to int
-    
-    if(neg==True):
+
+    if (x2 > pow(2,31)-1 or x2 < pow(-2,31) or x2 == 0): #check if 32 bit integer
+        return 0
+
+    if (neg==True):
         return x2*-1
 
     else:
         return x2
 
-num = reverse(-123)
+num = reverse(-12300)
 print(num)
 
 
@@ -48,6 +46,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_6(self):
         self.assertEqual(reverse(100900),9001)
+
+    def test_7(self):
+        self.assertEqual(reverse(901000),109)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
