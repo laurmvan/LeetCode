cbfc61887e0d5c1c6781fc21f0521f586a54d637
Lauren Van Vlierbergen <lvanvlierbergen@gmail.com>
2021-08-31 16:56:47 -0400

    string practice

---
+++ LongestCommon/lc_1.py
@@ -0,0 +1,56 @@
+import unittest
+
+def longestCommonPrefix(strs):
+    if len(strs) <= 1:
+        return strs[0]
+    
+    d1 = {} 
+
+    for word in strs:
+        if (len(word) > 2 and word[0:2] in d1):
+            d1[word[0:2]]+=1
+        elif (len(word) > 2):
+            d1[word[0:2]]=1
+        elif (word in d1):
+            d1[word]+=1 
+        else:
+            d1[word]=1
+
+
+    new_dict = sorted(d1, key = lambda word: word, reverse = True)
+
+    if (len(new_dict) == 1):
+        return new_dict[0]
+    if (d1[new_dict[0]] == d1[new_dict[1]]):
+            return ""
+    else: return new_dict[0]
+
+
+print(longestCommonPrefix(["dog","racecar","rack"]))
+
+
+#----------------------TEST CASES-----------------------------
+class TestStringMethods(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(longestCommonPrefix(["flower","flow","flight"]),"fl")
+
+    def test_2(self):
+        self.assertEqual(longestCommonPrefix(["dog","racecar","car"]),"")
+
+    def test_3(self):
+        self.assertEqual(longestCommonPrefix(["dog","racecar","rack"]),"ra")
+
+    def test_4(self):
+        self.assertEqual(longestCommonPrefix(["",""]),"")
+
+    def test_5(self):
+        self.assertEqual(longestCommonPrefix([""]),"")
+
+    def test_6(self):
+        self.assertEqual(longestCommonPrefix(["a"]),"a")
+
+    def test_6(self):
+        self.assertEqual(longestCommonPrefix(["a","an"]),"a")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
