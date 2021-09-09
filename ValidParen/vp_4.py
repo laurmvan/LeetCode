ef75972fdb679c674d72036fcac41b21c6e5fca2
Lauren Van Vlierbergen <lvanvlierbergen@gmail.com>
2021-09-08 23:10:31 -0400

    VP_4

--- ValidParen/vp_4.py
+++ ValidParen/vp_4.py
@@ -7,18 +7,32 @@ def isValid(s):
 
     dict_match = {"(":")",")":"(","{":"}","}":"{","[":"]","]":"["}
 
-    for symb in s:
-        s_list = list(s)
-        while (len(s_list) >= 0):
-            if (len(s_list) < 2):
-                break
-            # if(s_list[s_list.index(symb)])
-            if (len(s_list) >= 2 and s_list.index(symb) != len(s_list)):
-                if (s_list.index(symb))
+    s = list(s)
 
-            print(s_list.index(symb))
-            print(symb)
-            s_list=s_list[0:-1]
+    for i in range(len(s)):
+        if (i + 1 < len(s)):
+            # print (dict_match[s[i]])
+            print(s)
+            if (s[i + 1] == dict_match[s[i]]):
+                print("remove")
+                s.pop(i)
+                s.pop(i)
+                # s.pop(i+1)
+                print (s)
+                # print(s[i],s[i+1])
+
+    # for symb in s:
+    #     s_list = list(s)
+    #     while (len(s_list) >= 0):
+    #         if (len(s_list) < 2):
+    #             break
+    #         # if(s_list[s_list.index(symb)])
+    #         if (len(s_list) >= 2 and s_list.index(symb) != len(s_list)):
+    #             if (s_list.index(symb))
+
+    #         print(s_list.index(symb))
+    #         print(symb)
+    #         s_list=s_list[0:-1]
 
 
 
